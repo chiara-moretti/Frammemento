94bc7f1787af8ee747c0860f8a5627010aba09ab
chiara-moretti <chiara1.moretti@mail.polimi.it>
2026-04-08 15:19:43 +0200

    define and change state

--- src/lib/components/ClickCounter.svelte
+++ src/lib/components/ClickCounter.svelte
@@ -1,17 +1,19 @@
 <script>
-    console.log("Hello world")
-
-    let count = $state(0);
+    let props = $props();
+    // i props sono un oggetto che contiene tutte le proprietà passate al componente, quindi è possibile accedervi tramite $props() o tramite la sintassi $props.nomeProp
+    let { initialCount =$bindable} = props;
+    let count = $state(props.initialCount);
     // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
 
     let doubled = $derived(count*2);
     // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
-
 </script>
 
-<h3>Clicks Counter</h3>
+<ClickCounter bind:initialCount={initialCount}/>
+<h3>ClickCounter</h3>
 
-<button 
-    on:click={() => count++}>
+<button on:click={() => (count++)}>
     click: {count}
 </button>
+
+<span>doubled is {doubled}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,18 @@
 <script>
     console.log("Hello world")
 
-    let count = $state(0);
-    // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
-
-    let doubled = $derived(count*2);
-    // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
-
     import ClickCounter from "$lib/components/ClickCounter.svelte";
-    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
     // è possibile importare componenti da altri file e usarli come tag HTML
+
+    let count = $state(5);
 
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClickCounter />
-<ProgressiveSum />
+<ClickCounter initialCount={(count)}/>
+<p>count from parent: {count}</p>
 
 <style>
     h1 {
