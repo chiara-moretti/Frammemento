7941d2360b3c267b184b7987e2356b518584c7f6
chiara-moretti <chiara1.moretti@mail.polimi.it>
2026-04-08 16:06:13 +0200

    renaming $props

--- src/lib/components/ClickCounter.svelte
+++ src/lib/components/ClickCounter.svelte
@@ -1,9 +1,6 @@
 <script>
-    let props = $props();
+    let { initialCount: count } = $props();
     // i props sono un oggetto che contiene tutte le proprietà passate al componente, quindi è possibile accedervi tramite $props() o tramite la sintassi $props.nomeProp
-        
-    let count = $state(0);
-    // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
 
     let doubled = $derived(count * 2);
     // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
@@ -11,8 +8,8 @@
 
 <h3>ClickCounter</h3>
 
-<button onclick={() => count++}>
-    clicks: {count}
+<button onclick={() => initialCount++}>
+    clicks: {initialCount}
 </button>
 
 <span>doubled is {doubled}</span>
